71a959f4d93494e0f70f862538c37b74e3d4769c
kei <tenguuenglish@gmail.com>
2020-02-24 05:32:40 +0900

    :wrench: Setup base

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+const path = require('path');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'prettier', 'react', 'react-hooks'],
+  rules: {
+    // eslint
+    'consistent-return': ['off'],
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'import/prefer-default-export': ['off'],
+    'no-restricted-syntax': 'off',
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    // @typescript-eslint
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    //prettier
+    'prettier/prettier': [
+      'warn',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    // import
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    // jsx-a11y
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    // react
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/jsx-one-expression-per-line': ['warn'],
+    'react/jsx-wrap-multilines': ['off'],
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+        paths: ['src'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const argv = require('yargs').argv;
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  context: process.cwd(),
+  mode: argv.develop ? 'development' : 'production',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  devServer: {
+    open: true,
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    watchContentBase: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader', options: { transpileOnly: true } }],
+      },
+    ],
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin({ eslint: true })],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
