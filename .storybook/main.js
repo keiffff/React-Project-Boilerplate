e157789e1b59938a484c66baba787da3c46c2e3e
kei <tenguuenglish@gmail.com>
2020-07-14 07:05:40 +0900

    :wrench: Fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,26 +3,24 @@ const customConfig = require('../webpack.config.js');
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions/register', '@storybook/addon-knobs/register'],
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      resolve: customConfig.resolve,
-      module: {
-        ...config.module,
-        rules: [
-          ...config.module.rules,
-          {
-            test: /\.(ts|tsx)$/,
-            use: [
-              ...customConfig.module.rules[0].use,
-              {
-                loader: require.resolve('react-docgen-typescript-loader'),
-              },
-            ],
-          },
-        ],
-      },
-      plugins: [...config.plugins, ...customConfig.plugins],
-    };
-  },
+  webpackFinal: (config) => ({
+    ...config,
+    resolve: customConfig.resolve,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(ts|tsx)$/,
+          use: [
+            ...customConfig.module.rules[0].use,
+            {
+              loader: 'react-docgen-typescript-loader',
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [...config.plugins, ...customConfig.plugins],
+  }),
 };
